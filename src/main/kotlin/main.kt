643c8b0261d71b4517a3767ffa552d02321eb0fb
249725579@qq.com <XMCX@guo123>
2021-07-13 20:57:55 +0800

    feature: 增加usm锐化
    增加usm锐化

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,8 +7,8 @@ import java.io.FileOutputStream
 import java.nio.file.Files
 import javax.imageio.ImageIO
 
-fun extractPdfImg() {
-    val document = PDDocument.load(File("/media/lame/0DD80F300DD80F30/code/ok-pdf/src/main/resources/haskhell.pdf"))
+fun extractPdfImg(fp: String, baseFp: String) {
+    val document = PDDocument.load(File(fp))
     val pages = document.documentCatalog.pages.iterator()
     while (pages.hasNext()) {
         val page = pages.next()
@@ -20,7 +20,7 @@ fun extractPdfImg() {
                 if (imgObj is PDImageXObject) {
                     val image = imgObj.image
                     val metadata = imgObj.metadata
-                    FileOutputStream("/media/lame/0DD80F300DD80F30/code/ok-pdf/src/main/resources/hashhell/${System.currentTimeMillis()}.png").use {
+                    FileOutputStream("${baseFp}/haskhell/${System.currentTimeMillis()}.png").use {
                         ImageIO.write(image, "png", it)
                     }
                 }
@@ -29,19 +29,24 @@ fun extractPdfImg() {
     }
 }
 
-fun main(args: Array<String>) {
+fun mergeToPdf(baseFp: String) {
     println("开始pdf文件转换")
     PDDocument().use { doc ->
-        Files.list(File("/media/lame/0DD80F300DD80F30/code/ok-pdf/src/main/resources/hk").toPath())
-            .forEach { filePath ->
-                val page = PDPage()
-                doc.addPage(page)
-                val drawImage = PDImageXObject.createFromFile(filePath.toFile().absolutePath, doc)
-                val contents = PDPageContentStream(doc, page).use {
-                    it.drawImage(drawImage, 9f, 10f, 566f, 735f)
+        Files.list(File("${baseFp}/hk").toPath())
+                .forEach { filePath ->
+                    val page = PDPage()
+                    doc.addPage(page)
+                    val drawImage = PDImageXObject.createFromFile(filePath.toFile().absolutePath, doc)
+                    val contents = PDPageContentStream(doc, page).use {
+                        it.drawImage(drawImage, 9f, 10f, 566f, 735f)
+                    }
                 }
-            }
-        doc.save("/media/lame/0DD80F300DD80F30/code/ok-pdf/src/main/resources/haskhell3.pdf")
+        doc.save("${baseFp}/haskhell3.pdf")
         doc.close()
     }
+}
+
+fun main(args: Array<String>) {
+//    extractPdfImg("D:\\code\\ok-pdf\\src\\main\\resources\\haskhell.pdf","D:\\code\\ok-pdf\\src\\main\\resources")
+    mergeToPdf("D:\\code\\ok-pdf\\src\\main\\resources")
 }
--- srcipt/ruihua.py
+++ srcipt/ruihua.py
@@ -14,26 +14,45 @@ def ruihua(fp, op):
 
 def piplechain(fp, op):
     img = cv2.imread(fp, flags=cv2.IMREAD_GRAYSCALE)
-    ret,timg=cv2.threshold(img,200,255,cv2.THRESH_BINARY)
+
     # 创建 核
     # kernel = np.ones((2, 2), np.uint8)
     # 腐蚀
     # erorsion_img = cv2.erode(timg, kernel, iterations=1)
 
-    #膨胀
+    # 膨胀
     # erorsion_img = cv2.dilate(erorsion_img, kernel, iterations=1)
+    # 腐蚀
+    kernel = np.ones((1, 1), np.uint8)
+    # img = cv2.dilate(img, kernel, iterations=2)
+    img = cv2.erode(img, kernel, iterations=3)
+    ret, img = cv2.threshold(img, 180, 255, cv2.THRESH_BINARY)
+    img = cv2.dilate(img, kernel, iterations=2)
+    kernel = np.array([[0, -1, 0], [-1, 8, -1], [0, -1, 0]], np.float32)
+    img = cv2.filter2D(img, -1, kernel=kernel)
+    kernel = np.array([[0, -1, 0], [-1, 7, -1], [0, -1, 0]], np.float32)
+    img = cv2.filter2D(img, -1, kernel=kernel)
+    cv2.imwrite(op, img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 
 
-    kernel = np.array([[0, -1, 0], [-1, 6, -1], [0, -1, 0]], np.float32)
-    dst = cv2.filter2D(timg, -1, kernel=kernel)
-    cv2.imwrite(op, dst, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+# USM锐化公式表示如下：
+# （源图像– w*高斯模糊）/（1-w）；其中w表示权重（0.1～0.9），默认为0.6
+def usm(fp, op):
+    src = cv2.imread(fp)
+    blur_img = cv2.GaussianBlur(src, (0, 0), 5)
+    usm = cv2.addWeighted(src, 1.5, blur_img, -0.5, 0)
+    # h, w = src.shape[:2]
+    # result = np.zeros([h, w * 2, 3], dtype=src.dtype)
+    # result[0:h, 0:w, :] = src
+    # result[0:h, w:2 * w, :] = usm
+    cv2.imwrite(op, usm, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 
 
 if __name__ == "__main__":
-    hk = "/media/lame/0DD80F300DD80F30/code/ok-pdf/src/main/resources/hk"
-    for root, dirs, files in os.walk("/media/lame/0DD80F300DD80F30/code/ok-pdf/src/main/resources/hashhell",
+    hk = "D:/code/ok-pdf/src/main/resources/hk"
+    for root, dirs, files in os.walk("D:/code/ok-pdf/src/main/resources/haskhell",
                                      topdown=False):
         for name in files:
             print("\n正在处理图片" + os.path.join(root, name))
-            piplechain(os.path.join(root, name), os.path.join(hk, name))
+            usm(os.path.join(root, name), os.path.join(hk, name))
             print("\n图片存储" + os.path.join(hk, name))
